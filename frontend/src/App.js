c7bc557f5c9a4736dd23f9b995919a50b6986c48
Umer Mehmood <103528755+Umerrrdo@users.noreply.github.com>
2024-05-02 08:30:11 +0500

    signup-login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Nav from './components/Navbar';
+import LogIn from './pages/LogIn';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <Nav/>
+     
+      <LogIn />
     </div>
   );
 }

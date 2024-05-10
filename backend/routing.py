ea51eaaba616a5a1664c1ade27005ae2fa880abb
Syed Hussain Hamad Naqvi <100157373+HussainSyed268@users.noreply.github.com>
2024-05-11 01:49:00 +0500

    Feat:Django-removed :disappointed:

--- backend/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-# from .blocksmith import consumers
-from . import consumers
-
-websocket_urlpatterns = [
-    path('ws/central-server/', consumers.CentralServerConsumer.as_asgi()),
-]
--- backend/users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import Register,GetUser
-
-urlpatterns = [
-    path('register',Register.as_view()),
-    path('me',GetUser.as_view())
-]

a302e0d37d1876249f1dedf822b7c3fe4af3f4fa
Syed Hussain Hamad Naqvi <100157373+HussainSyed268@users.noreply.github.com>
2024-05-20 02:09:42 +0500

    feat:Network sidebar added

--- frontend/src/pages/Mining.jsx
+++ frontend/src/pages/Mining.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import BlockchainComponent from '../components/BlockChain';
+import Sidebar from '../components/Sidebar';
 
 function Mining() {
     return (
         <div>
 
             <BlockchainComponent />
+            <Sidebar />
+
         </div>
     );
 }
